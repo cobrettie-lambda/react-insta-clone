d0e32347915be89fbcf28d14d553111705eab392
Cobrettie <cobrettiegarner3@gmail.com>
2019-06-12 21:25:10 -0400

    PostContainer.js basic component functionality working

--- instagram-clone/src/App.js
+++ instagram-clone/src/App.js
@@ -1,20 +1,22 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+
 import './App.css';
+import dummyData from './dummy-data';
 import SearchBar from './components/SearchBar/SearchBar';
+import PostContainer from './components/PostContainer/PostContainer';
 
-class App extends Component {
-  constructor () {
-    super();
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <SearchBar />
-      </div>
-    )
-  }
-}
+function App() {
+  const [post] = useState(dummyData);
+  return (
+    <div className="App">
+      <SearchBar />
+      {post.map((userPost, index) => {
+        return (
+          <PostContainer key={index} post={userPost} />
+        );
+      })}
+    </div>
+  );
+};
 
 export default App;
--- instagram-clone/src/components/PostContainer/PostContainer.js
+++ instagram-clone/src/components/PostContainer/PostContainer.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
-import PostContainer.css;
+import './PostContainer.css';
 
 const PostContainer = props => {
     return (
         <div className="post-container">
             <div className="user-info">
-                <img className="profile-pic" src={props.post.thumbnailUrl} alt="Profile Image"></img>
+                <img className="profile-pic" src={props.post.thumbnailUrl} alt="Profile-pic"></img>
                 <p>{props.post.username}</p>
             </div>
             <div className="user-post">
-                <img src={props.post.imageUrl} alt="User posted image"></img>
+                <img src={props.post.imageUrl} alt="User-post"></img>
             </div>
         </div>
     );

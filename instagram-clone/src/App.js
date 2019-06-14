e58acd5788d788b11d7832266a255bab53daeb49
Cobrettie <cobrettiegarner3@gmail.com>
2019-06-13 22:48:57 -0400

    troubleshooting app.js class component

--- instagram-clone/src/App.js
+++ instagram-clone/src/App.js
@@ -1,22 +1,44 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import './App.css';
 import dummyData from './dummy-data';
 import SearchBar from './components/SearchBar/SearchBar';
 import PostContainer from './components/PostContainer/PostContainer';
 
-function App() {
-  const [post] = useState(dummyData);
-  return (
-    <div className="App">
-      <SearchBar />
-      {post.map((userPost, index) => {
-        return (
-          <PostContainer key={index} post={userPost} />
-        );
-      })}
-    </div>
-  );
-};
+// function App() {
+//   const [post] = useState(dummyData);
+//   return (
+//     <div className="App">
+//       <SearchBar />
+//       {post.map((userPost, index) => {
+//         return (
+//           <PostContainer key={index} post={userPost} />
+//         );
+//       })}
+//     </div>
+//   );
+// };
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      dummyData: []
+    }
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <SearchBar />
+        {this.state.dummyData.map((userPost, i) => {
+          return (
+            <PostContainer key={i} post={userPost} />
+          );
+        })}
+      </div>
+    );
+  }
+}
 
 export default App;

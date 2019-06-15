5ab5ba5ea9825cdad1f04746ce152a7e6da141b7
Cobrettie <cobrettiegarner3@gmail.com>
2019-06-14 20:13:41 -0400

    update app.js

--- instagram-clone/src/App.js
+++ instagram-clone/src/App.js
@@ -5,37 +5,19 @@ import dummyData from './dummy-data';
 import SearchBar from './components/SearchBar/SearchBar';
 import PostContainer from './components/PostContainer/PostContainer';
 
-// function App() {
-//   const [post] = useState(dummyData);
-//   return (
-//     <div className="App">
-//       <SearchBar />
-//       {post.map((userPost, index) => {
-//         return (
-//           <PostContainer key={index} post={userPost} />
-//         );
-//       })}
-//     </div>
-//   );
-// };
-
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      dummyData: []
-    }
+      posts: dummyData
+    };
   }
 
   render () {
     return (
       <div className="App">
         <SearchBar />
-        {this.state.dummyData.map((userPost, i) => {
-          return (
-            <PostContainer key={i} post={userPost} />
-          );
-        })}
+        <PostContainer posts={this.state.posts}/>
       </div>
     );
   }
--- instagram-clone/src/components/PostContainer/PostContainer.js
+++ instagram-clone/src/components/PostContainer/PostContainer.js
@@ -7,31 +7,6 @@ import CommentSection from '../CommentSection/CommentSection'
 
 const PostContainer = props => {
     return (
-        <div className="post-container">
-            <div className="user-info">
-                <img className="profile-pic" src={props.post.thumbnailUrl} alt="Profile-pic"></img>
-                <p>{props.post.username}</p>
-            </div>
-            <div className="user-post">
-                <img className="user-post-img" src={props.post.imageUrl} alt="User-post"></img>
-            </div>
-            <div className="interaction">
-                <div className="interact-icons">
-                    <IoIosHeartEmpty className="heartIcon" />
-                    <FaRegComment className="commentIcon" />
-                </div>
-                {props.post.likes} likes 
-            </div>
-            {props.post.comments.map((comment, index) => {
-                console.log("===", comment);
-                return (
-                    <CommentSection key={index} props={comment} />
-                );
-            })}
-            <div className="timeStamp">
-                {props.post.timestamp}
-            </div>
-        </div>
     );
 };
 
